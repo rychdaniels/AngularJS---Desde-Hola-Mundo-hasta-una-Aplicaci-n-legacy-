e7122ea1d21d621cc5b8c934d788f491c08e595a
RychDaniels <rychraya@gmail.com>
2021-07-21 20:35:13 -0500

    Creacion de rutas y controladores para cada una de las vistas

--- 06 Sitios Dinámicos - Responsive Sites/UniverApp/js/app.js
+++ 06 Sitios Dinámicos - Responsive Sites/UniverApp/js/app.js
@@ -5,4 +5,12 @@ app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
     // Variable para incluir el template del menu de navegacion
     $scope.menuSuperior = 'parciales/menu.html';
 
+    $scope.setActive = function(opcion){
+        $scope.mInicio = "";
+        $scope.mProfesores = "";
+        $scope.mAlumnos = "";
+
+        $scope[opcion] = 'active';
+    }
+
 }]);
--- 06 Sitios Dinámicos - Responsive Sites/UniverApp/js/config.js
+++ 06 Sitios Dinámicos - Responsive Sites/UniverApp/js/config.js
@@ -2,9 +2,18 @@ app.config( function($routeProvider){
 
     $routeProvider
         .when('/', {
-            templateUrl: 'parciales/home.html'
-    })
-    .otherwise({
-        redirectTo: '/'
-    });
+            templateUrl: 'parciales/home.html',
+            controller: 'inicioCtrl'
+        })
+        .when('/profesores', {
+            templateUrl: 'parciales/profesores.html',
+            controller: 'profesoresCtrl'
+        })
+        .when('/alumnos', {
+            templateUrl: 'parciales/alumnos.html',
+            controller: 'alumnosCtrl'
+        })
+            .otherwise({
+                redirectTo: '/'
+        });
 })
