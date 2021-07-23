c428118415979fa0b33e824f052236bc92e57b09
RychDaniels <rychraya@gmail.com>
2021-07-22 20:02:54 -0500

    Se cambia la obtencion de los datos con ayuda de un api externa en lugar de obtenerlo con PHP, esto debido a que php no regresaba ningu dato

--- 06 Sitios Dinamicos - Responsive Sites/UniverApp/js/alumnosCtrl.js
+++ 06 Sitios Dinamicos - Responsive Sites/UniverApp/js/alumnosCtrl.js
@@ -2,5 +2,13 @@ app.controller('alumnosCtrl', ['$scope','$http', function($scope,$http){
 	
 	$scope.setActive("mAlumnos");
 
+	$scope.alumnos = {};
+
+	// Traemos los arhivo de la BD
+	$http.get('https://jsonplaceholder.typicode.com/users')
+		.success(function(data){
+			$scope.alumnos = data;
+		});
+
 
 }]);
