d421070008de15373e5e6f5d7443200b6f247c6a
RychDaniels <rychraya@gmail.com>
2021-07-22 21:12:08 -0500

    Se agrega paginador para navegar entre los distintos registros que tiene como limite 5 registros por pagina

--- 06 Sitios Dinamicos - Responsive Sites/UniverApp/js/alumnosCtrl.js
+++ 06 Sitios Dinamicos - Responsive Sites/UniverApp/js/alumnosCtrl.js
@@ -3,14 +3,32 @@ app.controller('alumnosCtrl', ['$scope','$http', function($scope,$http){
 	$scope.setActive("mAlumnos");
 
 	$scope.alumnos = {};
+	$scope.posicion = 5;
 
 	// Traemos los arhivo de la BD
 	$http.get('https://jsonplaceholder.typicode.com/users')
 		.success(function(data){
-			console.log(data);
-			
 			$scope.alumnos = data;
 		});
+
+	/**
+	 * Funcion que nos lleva a la siguiente pagina
+	 */
+	$scope.siguientes = function(){
+
+		if( $scope.alumnos.length > $scope.posicion ){
+			$scope.posicion += 5
+		}
+	};
+
+	/**
+	 * Funcion que regresa a la pagina anterior
+	 */
+	$scope.anteriores = function(){
+		if( $scope.posicion > 5 ){
+			$scope.posicion -= 5
+		}
+	};
 
 
 }]);
